0ac6ade84d3d20d895b7442061a798a535eb49a3
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-07-22 18:28:00 +0100

    Introduction to forms using html code.

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Forms</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<form action="process_form.php">
+  <input name="search">
+  <button> Send </button>
+</form>
+
+
+</body>
+
+</html>
