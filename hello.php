31159c93ce1c341ee4e099cb2388f27712ece6f8
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-07-22 11:30:52 +0100

    testing import to git after save.

--- hello.php
+++ hello.php
@@ -1,4 +1,3 @@
-
 <?php
 
 $fruit = ['apple', 'banana', 'orange', 'mango'];
@@ -20,9 +19,9 @@ $fruit = ['apple', 'banana', 'orange', 'mango'];
       <?php endforeach; ?>
   </ol>
     </body>
+</html>
 
-<html>
-
+<h1>Using VarDump</h1>
 <!-- using Var Dump to see the content of a variable and type-->
 <?php
 $message = "hello Again";
@@ -40,6 +39,7 @@ $data=null;
 var_dump($is_admin, $data);
 
 ?>
+<h1>just Retriving data using index 2 output should be three</h1>
 <!-- retrieving data using PHP, at index of array 2-->
 
 <?php
@@ -49,6 +49,7 @@ var_dump ($articles[2]);
 
 
 
+<h1>Var dump autputing array, Data types </h1>
 
 <?php
 $count = 3;
@@ -70,6 +71,7 @@ var_dump($values);
 
 ?>
 
+<h1>Code for the Blog post functionality: array inside array </h1>
 <!--Code that uses vardump to output array , inside another array -->
 <?php
 
@@ -83,6 +85,7 @@ var_dump($articles);
 
 ?>
 
+<h1> Code for the Blog post functionality: For each loop</h1>
 <!-- Code for the Blog post functionality: For each loop  -->
 <?php
 
@@ -115,136 +118,13 @@ $fruit = ['apple', 'banana', 'orange', 'mango'];
 
     </body>
 </html>
+<h1>Ordered List using For Each Loop </h1>
 <!--Ordered List using For Each Loop -->
 1.	<ol>
 2.	    <?php foreach($fruit as $f): ?>
 3.	        <li><?= $f ?></li>
 4.	    <?php endforeach; ?>
 5.	</ol>
-<p>lol, I think I got a bug here. :P </p>
-=======
-<?php
+<p>lol, I think I got a bug here. :P</p>
 
-$fruit = ['apple', 'banana', 'orange', 'mango'];
-
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Fruit</title>
-    </head>
-    <body>
-
-    <h1>Fruit List</h1>
-
-    <ol>
-      <?php foreach($fruit as $f): ?>
-          <li><?= $f ?></li>
-      <?php endforeach; ?>
-  </ol>
-    </body>
-</html>
-
-<!-- using Var Dump to see the content of a variable and type-->
-<?php
-$message = "hello Again";
-echo $message;
-$count = 3;
-$price = 1.99;
-
-var_dump($message);
-var_dump($count);
-var_dump($price);
-
-$is_admin = true;
-$data=null;
-
-var_dump($is_admin, $data);
-
-?>
-<!-- retrieving data using PHP, at index of array 2-->
-
-<?php
-$articles = [ "first", "another", "three"];
-var_dump ($articles[2]);
-?>
-
-
-
-<!-- <h1>Var dump autputing array, Data types </h1> -->
-
-<?php
-$count = 3;
-$price = 9.99;
-$articles = [
-"message" => "hello World",
-"count" => 150,
-"pi" => 3.14,
-"status" => false,
-"result" => null
-];
-
-$values = [$count,$price];
-
-
-
-var_dump ($articles);
-var_dump($values);
-
-?>
-
-
-<!--Code that uses vardump to output array , inside another array -->
-<?php
-
-$articles = [
-["title" =>"First post", "content" => "this is the first post"],
-["title" =>"Another post", "content" => "Another Post to read here"],
-["title" =>"Third Post", "content" => "you must read this article!"],
-
-];
-var_dump($articles);
-
-?>
-
-<!-- Code for the Blog post functionality: For each loop  -->
-<?php
-
-$articles = [ "First Post", "second Post", "read This"];
-foreach($articles as $article){
-  echo $article, ", ";
-}
-
-?>
-
-<!-- Mixing HTML and PHP: Exercise from course solution-->
-<?php
-
-$fruit = ['apple', 'banana', 'orange', 'mango'];
-
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Fruit</title>
-    </head>
-    <body>
-
-    <h1>Fruit</h1>
-
-<?php if($fruit):?>
-  <p>frui exist</p>
-
-<?php endif; ?>
-
-    </body>
-</html>
-<!--Ordered List using For Each Loop -->
-1.	<ol>
-2.	    <?php foreach($fruit as $f): ?>
-3.	        <li><?= $f ?></li>
-4.	    <?php endforeach; ?>
-5.	</ol>
-
-
-</html>
+<!--test-->
