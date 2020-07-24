d5a0111750a60fbc7a168dc4a517c6c611722224
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-07-24 13:46:24 +0100

    Edit Functionality added.

--- new-article.php
+++ new-article.php
@@ -52,7 +52,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 header ("Location: article.php?id=$id");
                 exit;
 
-                      
+
             } else {
 
                 echo mysqli_stmt_error($stmt);
@@ -67,35 +67,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <h2>New article</h2>
 
-<?php if (! empty($errors)) : ?>
-    <ul>
-        <?php foreach ($errors as $error) : ?>
-            <li><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
-
-<form method="post">
-
-    <div>
-        <label for="title">Title</label>
-        <input name="title" id="title" placeholder="Article title" value="<?= htmlspecialchars ($title); ?>">
-    </div>
-
-    <div>
-        <label for="content">Content</label>
-        <textarea name="content" rows="4" cols="40" id="content"
-                  placeholder="Article content"><?= htmlspecialchars ($content); ?></textarea>
-    </div>
-
-    <div>
-        <label for="published_at">Publication date and time</label>
-        <input type="datetime-local" name="published_at" id="published_at"
-               value="<?= htmlspecialchars ($published_at); ?>">
-    </div>
-
-    <button>Add</button>
-
-</form>
-
+<?php require 'includes/article-form.php' ?>
 <?php require 'includes/footer.php'; ?>
