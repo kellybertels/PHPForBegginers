86f1548d47bde169d666cbd08e5f05418e7e371f
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-07-27 16:24:39 +0100

    Login correct working.

--- login.php
+++ login.php
@@ -1,20 +1,33 @@
 <?php
+require 'includes/url.php';
 
 session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  if ($_POST['username'] == 'kelly' && $_POST['password'] =='**123'){
+    $_SESSION['is_logged_in'] = true;
 
+    die("login CORRECT");
+
+  }else {
+  die("boo");
+
+  }
+}
 ?>
 <?php require 'includes/header.php'; ?>
 <h2>LOGIN</h2>
 
-<form>
+
+
+<form method="post">
 <div>
   <label for = "username">Username</label>
-<input name ="username" id"username">
+<input name= "username" id="username">
 </div>
 
 <div>
 <label for="password">Password</label>
-<input type="password" name="username" id="username">
+<input type="password" name="password" id="password">
 </div>
 
 <button>Log In</button>
