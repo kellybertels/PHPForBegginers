6867a85466930fe11b707d2b010395eafd66bee1
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-07-27 15:19:10 +0100

    Login Functionality added? (not sure it works how it should... )

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require 'includes/database.php';
 
+session_start();
+
 $conn = getDB();
 
 $sql = "SELECT *
@@ -18,7 +20,16 @@ if ($results === false) {
 
 ?>
 <?php require 'includes/header.php'; ?>
+<?php var_dump($_SESSION); ?>
+<?php if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in']): ?>
 
+    <p>You are logged in. <a href="logout.php">Log out</a></p>
+
+<?php else: ?>
+
+    <p>You are not logged in. <a href="login.php">Log in</a></p>
+
+<?php endif; ?>
 <a href="new-article.php">New article</a>
 
 <?php if (empty($articles)): ?>
