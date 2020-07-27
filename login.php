5c1b103eb6728753ec59a2c06d05b1577060b46c
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-07-27 15:43:29 +0100

    Login Form added, Not a secure method!

--- login.php
+++ login.php
@@ -2,4 +2,23 @@
 
 session_start();
 
-$_SESSION['is_logged_in'] = true;
+?>
+<?php require 'includes/header.php'; ?>
+<h2>LOGIN</h2>
+
+<form>
+<div>
+  <label for = "username">Username</label>
+<input name ="username" id"username">
+</div>
+
+<div>
+<label for="password">Password</label>
+<input type="password" name="username" id="username">
+</div>
+
+<button>Log In</button>
+
+</form>
+
+<?php require 'includes/footer.php';?>
