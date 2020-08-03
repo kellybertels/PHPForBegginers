a13eea38454eedc1e032ebc7c3dea3e5f1f7e3d2
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-08-03 11:37:44 +0100

    Magic function ___Dir__  added.

--- includes/init.php
+++ includes/init.php
@@ -4,7 +4,7 @@
  * Register and autoload, start and resume the sessions
  */
 spl_autoload_register(function($class){
-require "classes/{$class}.php";
+require dirname(__DIR__) . "/classes/{$class}.php";
 
 });
 
