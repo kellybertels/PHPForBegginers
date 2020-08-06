41f37c8825bddf9ebbaf28322bfe192434526cb9
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-08-06 10:24:36 +0100

    delete from database when uncheck box

--- classes/article.php
+++ classes/article.php
@@ -183,6 +183,24 @@ public function setCategories($conn, $ids)
 
             $stmt->execute();
         }
+        $sql = "DELETE FROM article_category2
+        WHERE article_id = {$this->id}";
+
+        if($ids){
+            $placeholders = array_fill(0,count($ids), '?');
+            $sql .=" AND category_id NOT IN (" . implode (", ", $placeholders) . ")";
+
+        }
+
+       
+        $stmt =$conn->prepare($sql);
+
+        foreach ($ids as $i => $id) {
+            $stmt->bindValue($i + 1, $id, PDO::PARAM_INT);
+        }
+
+
+        $stmt->execute();
     }
 
 
