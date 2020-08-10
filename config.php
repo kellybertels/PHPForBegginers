c855a8917944b37f21c701f69709ae2dccbf0baf
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-08-10 09:23:26 +0100

    Error Handling added

--- config.php
+++ config.php
@@ -10,3 +10,8 @@ define('SMTP_HOST', 'mail.example.com');
 define('SMTP_USER','user@example.com');
 define('SMTP_PASS','secret');
 
+
+//if set to true it will show the error details at the browser
+
+define('SHOW_ERROR_DETAIL', false);
+
