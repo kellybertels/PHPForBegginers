7c4bcdfbeddbc3d993a3bb1f6a24829135086b7b
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-08-13 13:31:53 +0100

    New user form created

---
+++ includes/new-user-form.php
@@ -0,0 +1,24 @@
+
+
+<form method="post">
+
+    <div>
+        <label for="username">Username</label>
+        <input name="username" id="username" placeholder="username" >
+    </div>
+
+    <div>
+        <label for="password">Password</label>
+        <input name="password" id="password" type = "password"  >
+    </div>
+
+    <div>
+        <label for="confirm-password">confirm your Password</label>
+        <input name="confirm-password" type = "password" id="confirm-password"  >
+    </div>
+
+ 
+
+    <button>Create</button>
+
+</form>
