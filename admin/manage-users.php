3f082eab4cce026314a2aa189940bb78c8efb23f
Kelly Nagy Bertels <kellynagybertels@gmail.com>
2020-08-13 13:32:54 +0100

    manage users page edited, user is getting added to db (basic working)

--- admin/manage-users.php
+++ admin/manage-users.php
@@ -1,6 +1,45 @@
+
+
 <?php
-//the REDIRECT FUNCTION IS HERE IN THE HEADER, TO REDIRECT TO ANOTHER PAGE WHEN A NEW ARTICLE IS ADDED.
+
+//page to manage the users, create a new user, 
+//TO DO: delete , view
 
 require '../includes/init.php';
 
 Auth::requireLogin();
+
+$user = new User();
+
+$conn = require '../includes/db.php';
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+
+    $confirm_password = $_POST['confirm-password'];
+    $user->username = $_POST['username'];
+    $user->password = $_POST['password'];
+    
+   
+    if ($user->create($conn)) {
+       
+
+        Url::redirect("/PHPForBegginers/admin/manage-users.php?");
+
+    }
+
+   
+ 
+
+    var_dump($user);
+}
+
+?>
+<?php require '../includes/header.php'; ?>
+
+<h2>Create a New User</h2>
+
+<?php require '../includes/new-user-form.php'; ?>
+
+<?php require '../includes/footer.php'; ?>
